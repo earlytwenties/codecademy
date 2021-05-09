a213a327393007552d352609ed00700cf9299e30
Maria Nikonova <mariatimberlake@Julio-Iglesias.local>
2021-05-09 19:04:36 +0100

    message
    I've done some practice on switch statements and operators

---
+++ practice.js
@@ -0,0 +1,35 @@
+/*let appleJuice = 'cold';
+if (appleJuice === 'cold') {console.log('refreshing!');
+} else {console.log('mulled wine\'s time!');
+}*/
+
+
+
+
+let appleJuice = 'cold'
+let season ='winter'
+if(appleJuice === 'cold' && season === 'winter') {console.log('Cool wintry juice, bro!');
+
+}
+
+let numberOfNipples = 2;
+if(numberOfNipples == 2) {console.log('Normal nips');
+} else if(numberOfNipples===3) {console.log('Too many nips, Meg!');
+} else {console.log('Not sure what\'s going on, here, guys!');
+
+}
+
+numberOfNipples === 1 ? console.log('nip missing') : console.log('haa haa');
+
+console.log('Adam'+' has'+ ' big knob.');
+
+
+let fruit = 'banana';
+switch (fruit) {
+case 'apple' : console.log('woopty doo');
+break;
+case 'cucumber' : console.log('not a fruit, bro');
+break;
+default : console.log('wtf');
+break;
+}
