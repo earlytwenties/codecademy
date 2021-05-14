cf652b21c2f8e495abf8e90183d54d4d2bc21ca4
Maria Timberlake <memarynikonova@gmail.com>
2021-05-14 11:04:18 +0100

    adding dog years calculator

---
+++ dog years calculator.js
@@ -0,0 +1,10 @@
+let myAge = 35; // sets variable for my age to 30;
+let earlyYears = 2; // sets variable for dog's early years
+earlyYears *=10.5;
+let laterYears = myAge - 2; // sets out dog's age minus the first 2 years
+laterYears *=4;
+
+let myAgeInDogYears = earlyYears + laterYears; // adds the dog's first two years and later years converted into human age
+let myName = 'Maria'.toUpperCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
